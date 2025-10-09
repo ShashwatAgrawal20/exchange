ef0a9769ae8ea3c73f8413785932f3a6ba9bb569
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2025-10-09 23:24:29 +0530

    fix(backend): register websocket handler and fix local port logging. - adding .ignore for Telescope user so that find_files don't pick that   submodule up - also adding `.clang-format`. FEEL FREE TO CHANGE THE RULES.
    Signed-off-by: Shashwat Agrawal <shashwatagrawal473@gmail.com>

--- backend/main.cpp
+++ backend/main.cpp
@@ -4,39 +4,37 @@ constexpr bool isUsingSSL = false;
 constexpr bool isServer = true;
 typedef uWS::WebSocket<isUsingSSL, isServer, SocketData> WebSocket;
 
-void socketOpenCallback(WebSocket* webSocket)
-{
-    std::cout << "Client connected" << std::endl; 
+void socketOpenCallback(WebSocket *webSocket) {
+    std::cout << "Client connected" << std::endl;
 }
-void socketMessageCallback(WebSocket* webSocket, std::string_view message, uWS::OpCode opCode)
-{
+void socketMessageCallback(WebSocket *webSocket, std::string_view message,
+                           uWS::OpCode opCode) {
     std::cout << "Received message: " << message << std::endl;
     webSocket->send(message, opCode);
 }
-void socketCloseCallback(WebSocket* webSocket, int closeCode, std::string_view message)
-{
+void socketCloseCallback(WebSocket *webSocket, int closeCode,
+                         std::string_view message) {
     std::cout << "Client disconnected" << std::endl;
 }
-void socketListenCallback(us_listen_socket_t* listenSocket)
-{
-    if(listenSocket) 
-    {
-        int localPort = us_socket_remote_port(0, (us_socket_t*)listenSocket);
-        std::cout << "WebSocket server listening on port: " << localPort << std::endl;
-    } 
-    else
-    {
+void socketListenCallback(us_listen_socket_t *listenSocket) {
+    if (listenSocket) {
+        int localPort =
+            us_socket_local_port(isUsingSSL, (us_socket_t *)listenSocket);
+        std::cout << "WebSocket server listening on port: " << localPort
+                  << std::endl;
+    } else {
         std::cout << "WebSocket server failed to listen" << std::endl;
     }
 }
-int main() 
-{
+int main() {
     uWS::App application = uWS::App();
 
     uWS::App::WebSocketBehavior<SocketData> webSocketBehaviour = {};
     webSocketBehaviour.open = &socketOpenCallback;
     webSocketBehaviour.message = &socketMessageCallback;
     webSocketBehaviour.close = &socketCloseCallback;
+
+    application.ws<SocketData>("/*", std::move(webSocketBehaviour));
 
     constexpr int listenPort = 7000;
     application.listen(listenPort, &socketListenCallback);
